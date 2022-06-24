d95538d7c1497854675db9df49b2de771b3cf97b
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-06-24 11:59:03 -0300

    Criando uma aplicação SpringBoot usando Spring Initializr V

--- 0x01/spring_boot_2/com/example/Demo2/HelloWorldResource.java
+++ 0x01/spring_boot_2/com/example/Demo2/HelloWorldResource.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/messages")
-public class HelloWorldResource {
+	@RequestMapping(value = "/messages")
+	public class HelloWorldResource {
 
-    @GetMapping("/home")
-    public String home() {
-        return "PROJETO MAVEN (SPRING INITIALIZR) CRIADO COM SUCESSO !!!";
+	    @GetMapping("/home")
+	    public String home() {
+	        return "PROJETO MAVEN (SPRING INITIALIZR) CRIADO COM SUCESSO !!!";
 
-    }
+	    }
+
 }
