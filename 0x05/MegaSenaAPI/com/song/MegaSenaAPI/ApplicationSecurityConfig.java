29226d61fe1ea1de3e53b3f794b8774fad1a9caa
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-06-28 16:15:01 -0300

    Construindo uma API REST com Spring Boot Security - MEGA SENA API

---
+++ 0x05/MegaSenaAPI/com/song/MegaSenaAPI/ApplicationSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.song.MegaSenaAPI;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+    @Configuration
+    @EnableWebSecurity
+    public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+                @Override
+                protected void configure(HttpSecurity http) throws Exception {
+                    http
+                            .httpBasic()
+                            .and()
+                            .authorizeRequests()
+                            .antMatchers("/h2-console/**").permitAll()
+                            .antMatchers("/").permitAll()
+                            .antMatchers("/books").hasRole("USER")
+                            .antMatchers("/books2").hasRole("ADMIN")
+                            .and()
+                            .csrf().disable()
+                            .headers().frameOptions().disable();
+                }
+
+
+
+
+    }
+
