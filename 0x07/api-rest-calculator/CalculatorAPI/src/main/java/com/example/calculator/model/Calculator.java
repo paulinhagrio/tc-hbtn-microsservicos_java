95a1dad12edfa54df212321d91e7f3781d587ecb
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-06-30 20:53:47 -0300

    Criando um aplicação API REST Calculator + JUNIT

---
+++ 0x07/api-rest-calculator/CalculatorAPI/src/main/java/com/example/calculator/model/Calculator.java
@@ -0,0 +1,67 @@
+package com.example.calculator.model;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Calculator {
+    public Double sum(Double number1, Double number2) {
+        if (number1 == null && number2 == null)
+            throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
+        return number1 + number2;
+    }
+
+    public Double sub(Double number1, Double number2) {
+        if (number1 == null && number2 == null)
+            throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
+        return number1 - number2;
+    }
+
+    public Double divide (Double number1, Double number2)  {
+       if (number1 == null && number2 == null){
+           throw new NullPointerException("Número 1 e número 2 são obrigatórios.");
+       }
+       if (number2 == 0){
+           throw new ArithmeticException("Divisão por zero não é permitido.");
+       }
+       return number1 / number2;
+    }
+
+    public Integer factorial(Integer factorial) {
+        if (factorial == null){
+            throw new NullPointerException("Número é obrigatório.");
+        }
+
+
+        Integer fact = 1;
+            for (int i=1; i<=factorial; i++){
+                fact = fact * i;
+            }
+            return fact;
+    }
+
+    // Exemplos:
+    // Integer = 1 -> Binary = 1
+    // Integer = 5 -> Binary = 101
+    // Integer = 20 -> Binary = 10100
+    public Integer integerToBinary(Integer integer) {
+        return Integer.valueOf(Integer.toBinaryString(integer));
+    }
+
+    // Exemplos:
+    // Integer = 1 -> Hexadecimal = "1"
+    // Integer = 5 -> Hexadecimal = "37"
+    // Integer = 170 -> Binary = "AA"
+    public String integerToHexadecimal(Integer integer) {
+        return Integer.toHexString(integer).toUpperCase();
+    }
+
+    // Exemplos
+    // Date 1 = LocalDate.of(2020, 3, 15);
+    // Date 2 = LocalDate.of(2020, 3, 29)
+    // Total de dias = 14
+    public int calculeDayBetweenDate(LocalDate date1, LocalDate date2) {
+        Period period = Period.between(date1, date2);
+
+        return period.getDays();
+    }
+}
---
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/model/CalculatorTest.java
@@ -0,0 +1,86 @@
+package com.example.calculator.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void setup(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    void sumTest() {
+        assertEquals(1000.0, calculator.sum(400.0, 600.0));
+    }
+
+    @Test
+    public void numbersNullSumTest() {
+        try{
+            calculator.sum(100.0, null);
+            fail("Missing Exception");
+        }catch(Throwable ex){
+            assertEquals(NullPointerException.class, ex.getClass());
+            assertEquals("Número 1 e número 2 são obrigatórios.", ex.getMessage());
+        }
+    }
+
+    @Test
+    void subTest() {
+        assertEquals(400.0, calculator.sub(1000.0, 600.0));
+    }
+
+    @Test
+    void divideTest() {
+        assertEquals(500.0, calculator.divide(1000.0, 2.0));
+    }
+
+    @Test
+    public void divisionByZeroTest() {
+        try{
+            calculator.divide(1000.0, 0.0);
+            fail("Missing Exception");
+        }catch(Throwable ex){
+            assertEquals(ArithmeticException.class, ex.getClass());
+            assertEquals("Divisão por zero não é permitido.", ex.getMessage());
+        }
+    }
+
+    @Test
+    void factorialTest() {
+        assertEquals(6, calculator.factorial(3));
+    }
+
+    @Test
+    void integerToBinaryTest() {
+        assertEquals(1, calculator.integerToBinary(1));
+        assertEquals(101, calculator.integerToBinary(5));
+        assertEquals(10100, calculator.integerToBinary(20));
+    }
+
+    @Test
+    void integerToHexadecimalTest() {
+        // Integer = 1 -> Hexadecimal = "1"
+        // Integer = 5 -> Hexadecimal = "37"
+        // Integer = 170 -> Binary = "AA"
+        assertEquals("1", calculator.integerToHexadecimal(1));
+        assertEquals("37", calculator.integerToHexadecimal(5));
+        assertEquals("AA", calculator.integerToHexadecimal(170));
+    }
+
+    @Test
+    void calculeDayBetweenDateTest() {
+        LocalDate Date1 = LocalDate.of(2020, 3, 15);
+        LocalDate Date2 = LocalDate.of(2020, 3, 29);
+        // Total de dias = 14
+
+        assertEquals(14, calculator.calculeDayBetweenDate(Date1, Date2));
+    }
+}
